13bf59abac84d2f98a781f0900257867815b2d64
Samridhi13Singh <h199443@dpshyderabad.com>
2025-05-10 18:05:52 +0530

    Update helpful.py

--- helpful.py
+++ helpful.py
@@ -1,7 +1,6 @@
 import streamlit as st
-from scipy.constants import G
-from scipy.constants import g
-from scipy.constants import c
+from scipy.constants import G,g,c
+
 def calculate_gravitational_force(mass1,mass2,r):
     return (G*mass1*mass2)/(r**2)
 
